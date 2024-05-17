ec6489d0451570197b42e076fc6c965b09d87f78
danilojduarte <76825919+danilojduarte@users.noreply.github.com>
2024-05-17 15:06:36 -0300

    Continuando : Aula tipos de variaveis

--- 002AulaJavaScript/aula01.js
+++ 002AulaJavaScript/aula01.js
@@ -1,2 +1,19 @@
-console.log("Hello word!");
-console.log("Iniciando estudos!")
+// Tipos de variaveis
+var nomeDoAluno = "Danilo Duarte";
+var idade = 49;
+var altura = 1.69;
+var estudante = true;
+
+console.log(nomeDoAluno, typeof nomeDoAluno);
+
+console.log(idade, typeof idade);
+console.log(altura, typeof altura);
+console.log(estudante, typeof estudante);
+
+
+
+
+
+// Como atribuir nome a uma variavel
+// var nomeDoAluno = "Danilo Duarte";
+// var idade = 49;
