7e5a10a02fc711c04fa9a58abd4f394086ff3870
danilojduarte <76825919+danilojduarte@users.noreply.github.com>
2024-05-20 15:26:18 -0300

    Aula sobre Coerção de titulos finalizada

--- 002AulaJavaScript/aula03EntradaDados.js
+++ 002AulaJavaScript/aula03EntradaDados.js
@@ -1,3 +1,22 @@
-const idade = prompt("Qual é sua idade?");
+// Coerção de titulos (Converção de titulos);
+// const idade = prompt.question("Qual e sua idade?");
+// const idade = prompt.question("Qual e sua idade?");
 
-console.log("A idade do usuario é =", idade, "Anos de idade");
+// Coerção Explicita (Conversão Manual)
+// const idadeNumber = Number(idade)
+
+// console.log(idadeNumber, typeof idadeNumber)
+// console.log(Number("x"));
+// console.log(String(10), typeof String(10));
+// console.log(Boolean(1));
+
+
+
+
+
+
+// const prompt = require('readline-sync')
+
+// const idade = prompt.question("Qual e sua idade?");
+
+// console.log("A idade do usuario é =", idade, "Anos de idade");
