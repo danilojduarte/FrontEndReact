c8f3a9564a0fe8ed2570579a9b5e86ce140854ad
danilojduarte <76825919+danilojduarte@users.noreply.github.com>
2024-05-20 14:39:27 -0300

    Entrada de dados com JavaScript

---
+++ 002AulaJavaScript/aula02Operadores.js
@@ -0,0 +1,24 @@
+// Obtendo raiz quadrada utilizando biblioteca {MATH}
+// const numero = 16;
+// const raizQuadrada = Math.sqrt(numero);
+// console.log(raizQuadrada);
+
+
+
+
+
+
+
+
+// Operações Matematicas
+// const nota1 = 10;
+// const nota2 = 10;
+
+// const somaDasNotas = nota1 + nota2;
+// let mediaDasNotas = somaDasNotas / 2;
+
+// mediaDasNotas = (nota1+nota2) / 2;
+
+
+// console.log('A soma das notas =', somaDasNotas);
+// console.log('A media das notas =', mediaDasNotas)
---
+++ 002AulaJavaScript/aula03EntradaDados.js
@@ -0,0 +1,3 @@
+const idade = prompt("Qual é sua idade?");
+
+console.log("A idade do usuario é =", idade, "Anos de idade");
