a7624c90816f1675b65699189b5c913d30842beb
danilojduarte <76825919+danilojduarte@users.noreply.github.com>
2024-05-22 04:51:32 -0300

    Finalizando aula de Switch Case

--- 002AulaJavaScript/aula04Comparacoes.js
+++ 002AulaJavaScript/aula04Comparacoes.js
@@ -1,11 +1,44 @@
+// Switch Card
+
+const permissoes = 'admin';
+
+switch (permissoes) {
+  case "aluno":
+    console.log("Você só pode visualizar as aulas.");
+    break;
+  case "professor":
+    console.log("Você pode alterar as aulas eadicionar exercícios.");
+    break;
+  case "admin":
+    console.log("Você pode fazer o que quiser no sistema.");
+    break;
+    default;
+    console.log("Não sei quem você é no sistema");
+}
+
+
+
+
+
+
+
+
 // Controle de Fluxos
-const prompt = require('readline-sync');
+// const prompt = require('readline-sync');
 
-const idade = Number(prompt.question('Qual eh a sua idade? '))
+// const idade = Number(prompt.question('Qual eh a sua idade? '))
 
-const ehMaiorDeIdade = idade >= 18;
+// const ehMaiorDeIdade = idade >= 18;
 
+// const mediaDoAluno = 4.999;
 
+// if (mediaDoAluno >= 7) {
+//   console.log("Aprovado!");
+// } else if (mediaDoAluno >= 5) {
+//   console.log("Prova Final");
+// } else {
+//   console.log("Reprovador");
+// }
 
 
 
