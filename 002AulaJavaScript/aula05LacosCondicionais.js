40510cd35c10505762bec329916fa9740b8a0c94
danilojduarte <76825919+danilojduarte@users.noreply.github.com>
2024-05-23 14:35:12 -0300

    Programa pagrama para gerar número aleatorio

--- 002AulaJavaScript/aula05LacosCondicionais.js
+++ 002AulaJavaScript/aula05LacosCondicionais.js
@@ -1,19 +1,29 @@
 const prompt = require("readline-sync");
-let notaDoAluno = Number(prompt.question('Informe a nota do aluno: '));
+// Programa parama para gerar número aleatorio - exemplo03
 
-let somaDasNotas = 0; //Acumulador para saber o valor total das notas;
 let qtdNotasValidas = 0; //Acmulador para saber a quantidade de notas;
+const numeroAleatorio = parseInt(Math.random() * 10);
 
-while (notaDoAluno >= 0) {
-  somaDasNotas += notaDoAluno; //Incrementando o acumulador (somaDasNotas)
-  notaDoAluno = Number(prompt.question("Informe a proxima nota: "));
-  qtdNotasValidas++; //Pos incremento;
-}
+console.log(numeroAleatorio);
 
-console.log(somaDasNotas);
-console.log(qtdNotasValidas);
 
-console.log("Medias das notas é = ", somaDasNotas / qtdNotasValidas);
+
+
+// let notaDoAluno = Number(prompt.question('Informe a nota do aluno: '));
+
+// let somaDasNotas = 0; //Acumulador para saber o valor total das notas;
+// let qtdNotasValidas = 0; //Acmulador para saber a quantidade de notas;
+
+// while (notaDoAluno >= 0) {
+//   somaDasNotas += notaDoAluno; //Incrementando o acumulador (somaDasNotas)
+//   notaDoAluno = Number(prompt.question("Informe a proxima nota: "));
+//   qtdNotasValidas++; //Pos incremento;
+// }
+
+// console.log(somaDasNotas);
+// console.log(qtdNotasValidas);
+
+// console.log("Medias das notas é = ", somaDasNotas / qtdNotasValidas);
 
 
 
--- 002AulaJavaScript/tempCodeRunnerFile.js
+++ 002AulaJavaScript/tempCodeRunnerFile.js
@@ -1,16 +1,3 @@
-const prompt = require("readline-sync");
-let notaDoAluno = Number(prompt.question('Informe a nota do aluno: '));
+const numeroAleatorio = parseInt(Math.random() * 10);
 
-let somaDasNotas = 0; //Acumulador para saber o valor total das notas;
-let qtdNotasValidas = 0; //Acmulador para saber a quantidade de notas;
-
-while (notaDoAluno >= 0) {
-  somaDasNotas += notaDoAluno; //Incrementando o acumulador (somaDasNotas)
-  notaDoAluno = Number(prompt.question("Informe a proxima nota: "));
-  qtdNotasValidas++; //Pos incremento;
-}
-
-console.log(somaDasNotas);
-console.log(qtdNotasValidas);
-
-console.log("Medias das notas é = ", somaDasNotas / qtdNotasValidas);
+console.log(numeroAleatorio);
