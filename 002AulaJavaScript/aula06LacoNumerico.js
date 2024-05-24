d64e1a5a00513f6374533a0cfd5909c374013521
danilojduarte <76825919+danilojduarte@users.noreply.github.com>
2024-05-24 14:59:00 -0300

    Incrementando for

--- 002AulaJavaScript/aula06LacoNumerico.js
+++ 002AulaJavaScript/aula06LacoNumerico.js
@@ -1,1 +1,17 @@
+const prompt = require("readline-sync");
+
 // Estrutura do For 
+
+let i = 0;
+//Expressao 1: Inicialização da variável de controle;
+//Expressao 2: Condição de permanência do for;
+//Expressao 3: P incremento da variável de controle;
+
+
+// for (expressao 1; expressao 2; expressao 3;) {
+  //  O escorpo do for
+// }
+
+for (i = 0; i > 5; i++) {
+  
+}
