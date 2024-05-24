45e6682046440385485a0baa59d9fcaa2750eb46
danilojduarte <76825919+danilojduarte@users.noreply.github.com>
2024-05-24 15:30:34 -0300

    Trabalhando com estrutura controlada

--- 002AulaJavaScript/aula06LacoNumerico.js
+++ 002AulaJavaScript/aula06LacoNumerico.js
@@ -1,17 +1,34 @@
 const prompt = require("readline-sync");
+//Exemplo - 2
+
+let maiorNumero = 0;
+let numerInformado;
+
+for (let i = 1; i <= 5; i++) {
+  numerInformado = Number(prompt.question('Informe um numero positivo: '));
+
+  if (numerInformado > maiorNumero) {
+    maiorNumero = numerInformado;
+  }
+}
+
+console.log("Maior numero:", maiorNumero);
+
+
 
 // Estrutura do For 
-
-let i = 0;
+//Exemplo - 1
+// let a;
 //Expressao 1: Inicialização da variável de controle;
 //Expressao 2: Condição de permanência do for;
 //Expressao 3: P incremento da variável de controle;
+//Será possivel criar a variavel dentro do for ex : for (let a = 0; a > 5)
 
 
 // for (expressao 1; expressao 2; expressao 3;) {
   //  O escorpo do for
 // }
 
-for (i = 0; i > 5; i++) {
-  
-}
+// for (a = 0; a < 5; a++) {
+//   console.log(a);
+// }
--- 002AulaJavaScript/tempCodeRunnerFile.js
+++ 002AulaJavaScript/tempCodeRunnerFile.js
@@ -1,5 +1,15 @@
-const prompt = require("readline-sync");
+// Estrutura do For 
 
-while (true) {
-  console.log("Olá, mundo");
+let a;
+//Expressao 1: Inicialização da variável de controle;
+//Expressao 2: Condição de permanência do for;
+//Expressao 3: P incremento da variável de controle;
+
+
+// for (expressao 1; expressao 2; expressao 3;) {
+  //  O escorpo do for
+// }
+
+for (a = 0; a < 5; a++) {
+  console.log(a);
 }
