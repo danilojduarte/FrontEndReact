c8f12fd57e71c37a127f94b882f65b2fff48ed0f
danilojduarte <76825919+danilojduarte@users.noreply.github.com>
2024-05-28 14:44:48 -0300

    Finalizada aula 01 de Funções

--- 002AulaJavaScript/aula07Funcoes.js
+++ 002AulaJavaScript/aula07Funcoes.js
@@ -1,5 +1,14 @@
 const prompt = require("readline-sync");
-//Funções {1} 
-function saudacao() {
-  
+
+//Funções {1} - Finalizado parte 1 - Iniciando parte II
+function saudacao(nomeDoEstudante, curso = "Front-end em React") {
+  //tamplate string Ou tamplete literals
+
+  return `Olá, ${nomeDoEstudante}! Seja bem vindo ao curso de ${curso}`;
 }
+
+const mensagemDeSaudacao = saudacao("Danilo", "React"); // Chamada da function saudacao
+
+console.log(mensagemDeSaudacao);
+
+
